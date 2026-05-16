bd00139d457cb4f40e31ab533cfb9188aa27a1b9
Avinash Prasad <avinashprasad4608@gmail.com>
2026-05-16 21:14:21 +0530

    separate server

--- server/Backend/src/utils/response.js
+++
@@ -1,7 +0,0 @@
-export const success = (res, message, data) => {
-    return res.status(200).json({ status: true, message, data });
-};
-
-export const error = (res, message, code = 400) => {
-    return res.status(code).json({ status: false, message });
-};
--- server/Backend/src/utils/sendEmail.js
+++
@@ -1,8 +0,0 @@
-// src/utils/sendEmail.js
-
-// Simple stub for now – replace with SES / Nodemailer in production
-export const sendEmail = async (to, body) => {
-  console.log(`📧 [DEV] Sending email to ${to}: ${body}`);
-  // TODO: integrate AWS SES or any SMTP provider here
-  return true;
-};
